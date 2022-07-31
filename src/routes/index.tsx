b0bd085750be12b3d46d3fadc5464ee462770b6b
lazy-sky <1cielo@naver.com>
2022-08-01 00:09:19 +0900

    Feat: Set routes about Meeting

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './Home'
+import Meeting from './Meeting'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='meeting'>
+          <Route path=':name'>
+            <Route path=':id' element={<Meeting />} />
+          </Route>
+        </Route>
       </Routes>
     </div>
   )
